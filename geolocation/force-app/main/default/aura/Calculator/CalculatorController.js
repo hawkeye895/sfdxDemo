ef1a254ae1efe5424bb4b436a9ed2a7f116686aa
prathamesh patil <prathameshpatil895@gmail.com>
2020-05-24 00:24:16 +0530

    kk

--- geolocation/force-app/main/default/aura/Calculator/CalculatorController.js
+++ geolocation/force-app/main/default/aura/Calculator/CalculatorController.js
@@ -1,5 +1,29 @@
 ({
-    myAction : function(component, event, helper) {
+    doAdd: function (component, event, helper) {
+        var num1 = component.get('v.input1');
+        var num2 = component.get('v.input2');
+        //alert(parseInt(num1) + parseInt(num2));
+        component.set('v.result', parseInt(num1) + parseInt(num2));
+    },
+    doSub: function (component, event, helper) {
+        var num1 = component.get('v.input1');
+        var num2 = component.get('v.input2');
+        //alert(parseInt(num1) - parseInt(num2));
+        component.set('v.result', parseInt(num1) - parseInt(num2));
 
-    }
+    },
+    doMul: function (component, event, helper) {
+        var num1 = component.get('v.input1');
+        var num2 = component.get('v.input2');
+       // alert(parseInt(num1) * parseInt(num2));
+        component.set('v.result', parseInt(num1) * parseInt(num2));
+
+    },
+    doDiv: function (component, event, helper) {
+        var num1 = component.get('v.input1');
+        var num2 = component.get('v.input2');
+       // alert(parseInt(num1) / parseInt(num2));
+        component.set('v.result', parseInt(num1) / parseInt(num2));
+
+    },
 })
